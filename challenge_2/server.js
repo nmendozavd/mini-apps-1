f60753a384161d4b8ea65eff246749efc57874bc
Noel Mendoza <nmendozavd@gmail.com>
2020-01-14 18:23:29 -0800

    Created POST console log on server and Post request on client

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,26 @@
+const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express()
+const morgan = require('morgan');
+const port = 3000
 
 
+app.get('/', (req, res) => {
+  // res.render('form');
+  res.sendFile(path.join(__dirname, './client/index.html'))
+})
+
+
+app.post('/', (req, res) => {
+  console.log('Logging this on Server?');
+  console.log(req.body);
+  res.send('Got a Post Request')
+
+})
+
+// app.use(morgan('dev')); // research this?
+// for parsing application/json
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../client/')))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
